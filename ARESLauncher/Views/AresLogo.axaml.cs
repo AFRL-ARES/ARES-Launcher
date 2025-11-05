47def47b671c4f78ec27160688c5996032c9767a
Arnas Babeckis <ababeckis@dcscorp.com>
2025-11-05 13:25:39 -0500

    Made so that the logo follows the theme

---
+++ ARESLauncher/Views/AresLogo.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ARESLauncher;
+
+public partial class AresLogo : UserControl
+{
+    public AresLogo()
+    {
+        InitializeComponent();
+    }
+}
