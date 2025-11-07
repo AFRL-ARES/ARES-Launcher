8d433b14c5d3d1401c01651ae9570545eea251b6
Babeckis, Arnas <xxl606@gmail.com>
2025-11-07 18:38:58 -0500

    Added an update button

--- ARESLauncher/Tools/VersionExtensions.cs
+++ ARESLauncher/Tools/VersionExtensions.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using NuGet.Versioning;
+using System.Collections.Generic;
 using System.Linq;
-using NuGet.Versioning;
 
 namespace ARESLauncher.Tools;
 
@@ -9,13 +9,13 @@ public static class VersionExtensions
   public static bool IsGreatest(this SemanticVersion version, IEnumerable<SemanticVersion> versionsToCheck)
   {
     var versions = versionsToCheck.ToArray();
-    if (!versions.Any())
+    if(!versions.Any())
       return true;
 
     var latest = versions[0];
-    for (var i = 1; i < versions.Length; i++)
-      if (versions[i] > latest)
+    for(var i = 1; i < versions.Length; i++)
+      if(versions[i] > latest)
         latest = versions[i];
-    return version > latest;
+    return version >= latest;
   }
 }
