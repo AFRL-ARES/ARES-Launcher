4906b8a53d89fc6f13ca34c64217b8ee665601fb
Babeckis, Arnas <xxl606@gmail.com>
2025-11-20 14:51:19 -0500

    Added a check to make sure you can't have multiple launchers instances.

--- ARESLauncher/App.axaml.cs
+++ ARESLauncher/App.axaml.cs
@@ -1,16 +1,22 @@
-using System;
 using ARESLauncher.ViewModels;
 using ARESLauncher.Views;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ARESLauncher;
 
 public class App : Application
 {
+  private const string AppMutexName = "6293b4fc-466d-4135-ad8c-7ed25c6840c2";
+  private static Mutex? _mutex;
+
   public bool IsShuttingDown { get; private set; }
 
   public static IServiceProvider? ServiceProvider { get; private set; }
@@ -22,6 +28,13 @@ public class App : Application
 
   public override void OnFrameworkInitializationCompleted()
   {
+    _mutex = new Mutex(true, AppMutexName, out var createdNew);
+    if(!createdNew)
+    {
+      ShowInstanceAlreadyRunningPopup();
+      return;
+    }
+
     var collection = new ServiceCollection();
     collection.AddCommonServices();
     var services = collection.BuildServiceProvider();
@@ -39,11 +52,20 @@ public class App : Application
 
       desktop.ShutdownRequested += (_, _) =>
       {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+        _mutex = null;
         // TODO: Double check this works on Windows
         IsShuttingDown = true;
       };
 
-      desktop.Exit += (_, _) => { IsShuttingDown = true; };
+      desktop.Exit += (_, _) => 
+      {
+        _mutex?.ReleaseMutex();
+        _mutex?.Dispose();
+        _mutex = null;
+        IsShuttingDown = true; 
+      };
     }
     else if(ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
     {
@@ -59,5 +81,57 @@ public class App : Application
   public void BeginShutdown()
   {
     IsShuttingDown = true;
+  }
+
+  private async void ShowInstanceAlreadyRunningPopup()
+  {
+    // Get a reference to the desktop lifetime
+    if(ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop)
+    {
+      return;
+    }
+
+    // Create a simple window to act as a popup.
+    var popup = new Window
+    {
+      Title = "Application Error",
+      Width = 400,
+      Height = 150,
+      WindowStartupLocation = WindowStartupLocation.CenterScreen,
+      Topmost = true, // Ensure it's seen
+      Content = new StackPanel
+      {
+        Spacing = 10,
+        Margin = new Thickness(20),
+        VerticalAlignment = VerticalAlignment.Center,
+        HorizontalAlignment = HorizontalAlignment.Center,
+        Children =
+        {
+          new TextBlock
+          {
+              Text = "Launcher Already Running",
+              FontSize = 16,
+              FontWeight = Avalonia.Media.FontWeight.Bold,
+              HorizontalAlignment = HorizontalAlignment.Center
+          },
+          new TextBlock
+          {
+              Text = "Another instance of the ARES Launcher is already running. This instance will now close.",
+              HorizontalAlignment = HorizontalAlignment.Center,
+              TextWrapping = Avalonia.Media.TextWrapping.Wrap
+          }
+        }
+      }
+    };
+
+    // Show the popup.
+    popup.Show();
+
+    // Give the user a few seconds to read the message.
+    await Task.Delay(3000);
+
+    // Close the popup and shut down the application.
+    popup.Close();
+    desktop.Shutdown();
   }
 }
